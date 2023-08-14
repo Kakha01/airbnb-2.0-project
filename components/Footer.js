76ceba2b96380f1672d7b0829d51294a36a30b84
Kakha <k.diasamidze2005@gmail.com>
2023-08-14 12:56:03 +0400

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+
+
+export default function Footer() {
+  return (
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-16 md:px-32 py-14 bg-gray-100 md:space-y-0 text-gray-600">
+          <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airbnb works</p>
+            <p>News room</p>
+            <p>Investors</p>
+            <p>Airbnb Plus</p>
+            <p>Airbnb Luxe</p>
+          </div>
+
+
+          <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome clone</p>
+            <p>Referrals accepted</p>
+            <p>Airbnb Post</p>
+          </div>
+
+
+
+          <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>Help Centre</p>
+            <p>Trust &amp; Safety</p>
+            <p>Say Hi Youtube</p>
+            <p>Easter Eggs</p>
+            <p>For the Win</p>
+          </div>
+
+          <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>Random Thing</p>
+            <p>Is not</p>
+            <p>Being done</p>
+            <p>Full stack hero</p>
+            <p>Join Now</p>
+          </div>
+    </footer>
+  )
+}
